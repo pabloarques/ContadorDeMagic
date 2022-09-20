6e761ab59d43f92410b33d9fc2441ec8a48fb16a
Pablo <pabloarques98@gmail.com>
2022-09-20 17:26:18 +0200

    casa1

--- app/src/main/java/com/example/contadordemagic/FirstFragment.java
+++ app/src/main/java/com/example/contadordemagic/FirstFragment.java
@@ -4,39 +4,56 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.contadordemagic.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private Button btnPmas;
+    private Button btnPMas2;
+    private Button btnPMenos;
+    private Button btnPMenos2;
+    private ImageButton imgCoraL;
+    private ImageButton imgCoraL2;
+    private ImageButton imgCoraV;
+    private ImageButton imgCoraV2;
+
+    private int vida1;
+    private int veneno1;
+    private int vida2;
+    private int veneno2;
+
+
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        reset();
         return binding.getRoot();
-
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void reset(){
+        vida1=20;
+        vida2=20;
+        veneno1=0;
+        veneno2=0;
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+        updateScore();
     }
+
+    private void updateScore() {
+        //contador
+    }
+
 
     @Override
     public void onDestroyView() {
--- app/src/main/java/com/example/contadordemagic/MainActivity.java
+++ app/src/main/java/com/example/contadordemagic/MainActivity.java
@@ -36,14 +36,10 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
